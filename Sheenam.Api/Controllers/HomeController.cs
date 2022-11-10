7e97fefb7d4cbaf29ca29ab3220cd5755d4e0f69
khatanboyevelbek <programmerelbek@gmail.com>
2022-11-10 04:02:16 -0800

    INFRA: Initialize Web API Project

---
+++ Sheenam.Api/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿// ---------------------------------------------------
+// Copyright (c) Coalition of Good-hearted Engineers
+// Free to use comfort and pease
+// ---------------------------------------------------
+
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace Sheenam.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : RESTFulController
+    {
+        [HttpGet]
+        public ActionResult<string> Get() => Ok("Hi Elbek!");
+    }
+}
