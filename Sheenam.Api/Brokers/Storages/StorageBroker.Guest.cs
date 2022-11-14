ae9319e33051a48ae74425045b947f3807cf3f5d
khatanboyevelbek <programmerelbek@gmail.com>
2022-11-14 21:33:29 +0500

    DATA: Guests Model & Migrate

---
+++ Sheenam.Api/Brokers/Storages/StorageBroker.Guest.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sheenam.Api.Models.Foundations.Guests;
+
+namespace Sheenam.Api.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<Guest> Guests { get; set; }
+    }
+}
