df6a5f8264b12d0f5d337110abc75e155ca409d5
khatanboyevelbek <programmerelbek@gmail.com>
2022-11-29 21:45:42 +0500

    Bug fixed

--- Sheenam.Api/Models/Foundations/Guests/Guest.cs
+++ Sheenam.Api/Models/Foundations/Guests/Guest.cs
@@ -8,12 +8,12 @@ namespace Sheenam.Api.Models.Foundations.Guests
     public class Guest
     {
         public Guid Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public DateTimeOffset DateOfBirth { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Address { get; set; }
-        public GenderType Gender { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTimeOffset? DateOfBirth { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Address { get; set; }
+        public GenderType? Gender { get; set; }
     }
 }
