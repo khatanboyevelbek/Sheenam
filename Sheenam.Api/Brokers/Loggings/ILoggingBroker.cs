d35ddd429210bb3b531ff7e523097d5ae7804aba
khatanboyevelbek <programmerelbek@gmail.com>
2022-12-01 23:07:35 +0500

    BROKERS: Implamenting Logging

---
+++ Sheenam.Api/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,13 @@
+﻿// ---------------------------------------------------
+// Copyright (c) Coalition of Good-hearted Engineers
+// Free to use to find comfort and pease
+// ---------------------------------------------------
+
+namespace Sheenam.Api.Brokers.Loggings
+{
+    public interface ILoggingBroker
+    {
+        void LogError(Exception exception);
+        void LogCritical(Exception exception);
+    }
+}
