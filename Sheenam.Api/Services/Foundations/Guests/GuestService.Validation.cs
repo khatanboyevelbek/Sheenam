69f377b01ef69e18aea77ac3c7208b91be611a17
khatanboyevelbek <programmerelbek@gmail.com>
2022-12-27 12:23:53 +0500

    DATA: Added Password Validation

--- Sheenam.Api/Services/Foundations/Guests/GuestService.Validation.cs
+++ Sheenam.Api/Services/Foundations/Guests/GuestService.Validation.cs
@@ -21,6 +21,7 @@ public partial class GuestService
           (Rule: IsInvalid(guest.LastName), Parameter: nameof(guest.LastName)),
           (Rule: IsInvalid(guest.DateOfBirth), Parameter: nameof(guest.DateOfBirth)),
           (Rule: IsInvalid(guest.Email), Parameter: nameof(guest.Email)),
+          (Rule: IsInvalid(guest.Password), Parameter: nameof(guest.Password)),
           (Rule: IsInvalid(guest.Address), Parameter: nameof(guest.Address)),
           (Rule: IsInvalid(guest.Gender), Parameter: nameof(guest.Gender)));
     }
