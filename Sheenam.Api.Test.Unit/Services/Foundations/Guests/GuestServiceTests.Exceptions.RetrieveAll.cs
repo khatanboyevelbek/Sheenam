fc6e02d3c4878c562c5f42488e6c95d7634280cb
khatanboyevelbek <programmerelbek@gmail.com>
2022-12-28 19:10:52 +0500

    ShouldThrowExceptionOnRetrieveAllIfSqlErrorOccured() -> FAIL

---
+++ Sheenam.Api.Test.Unit/Services/Foundations/Guests/GuestServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,48 @@
+﻿// ---------------------------------------------------
+// Copyright (c) Coalition of Good-hearted Engineers
+// Free to use to find comfort and pease
+// ---------------------------------------------------
+
+using Microsoft.Data.SqlClient;
+using Moq;
+using Sheenam.Api.Models.Foundations.Guests;
+using Sheenam.Api.Models.Foundations.Guests.Exceptions;
+using Xunit;
+
+namespace Sheenam.Api.Test.Unit.Services.Foundations.Guests
+{
+    public partial class GuestServiceTests
+    {
+        [Fact]
+        public void ShouldThrowExceptionOnRetrieveAllIfSqlErrorOccured()
+        {
+            // given
+            SqlException sqlException = GetSqlError();
+
+            var failedGuestStorageException =
+                new FailedGuestStorageException(sqlException);
+
+            var guestDependencyException =
+                new GuestDependencyException(failedGuestStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllGuests()).Throws(sqlException);
+
+            // when 
+            Action retrieveAllGuestsAction = () => this.guestService.RetrieveAllGuests();
+
+            // then
+            Assert.Throws<GuestDependencyException>(retrieveAllGuestsAction);
+
+            this.storageBrokerMock.Verify(broker => 
+                broker.SelectAllGuests(), Times.Once());
+
+            this.loggingBrokerMock.Verify(broker => 
+                broker.LogCritical(It.Is(SameExceptionAs(guestDependencyException))), 
+                    Times.Once());
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
