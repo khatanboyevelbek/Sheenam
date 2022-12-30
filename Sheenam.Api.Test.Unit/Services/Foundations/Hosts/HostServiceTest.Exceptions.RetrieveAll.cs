37a3aa422e84558c2eb916e99ff222ef5ee1c5a6
khatanboyevelbek <programmerelbek@gmail.com>
2022-12-30 21:27:40 +0500

    ShouldThrowExceptionOnRetrieveAllIfSqlErrorOcurred() -> FAIL

---
+++ Sheenam.Api.Test.Unit/Services/Foundations/Hosts/HostServiceTest.Exceptions.RetrieveAll.cs
@@ -0,0 +1,35 @@
+﻿// ---------------------------------------------------
+// Copyright (c) Coalition of Good-hearted Engineers
+// Free to use to find comfort and pease
+// ---------------------------------------------------
+
+using Host = Sheenam.Api.Models.Foundations.Hosts.Host;
+using Xunit;
+using Microsoft.Data.SqlClient;
+using Sheenam.Api.Models.Foundations.Hosts.Exceptions;
+
+namespace Sheenam.Api.Test.Unit.Services.Foundations.Hosts
+{
+    public partial class HostServiceTest
+    {
+        [Fact]
+        public void ShouldThrowExceptionOnRetrieveAllIfSqlErrorOcurred()
+        {
+            // given
+            SqlException sqlException = GetSqlError();
+
+            var failedHostStorageException = 
+                new FailedHostStorageException(sqlException);
+
+            var hostDependencyException =
+                new HostDependencyException(failedHostStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllHosts()).Throws(sqlException);
+
+            // when & then
+            Assert.Throws<HostDependencyException>(() => 
+                this.hostservice.RetrieveAllHosts());
+        }
+    }
+}
