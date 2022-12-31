254167f5fd981c93b9ec7fedaf0d961a53853d2f
khatanboyevelbek <programmerelbek@gmail.com>
2022-12-31 16:19:41 +0500

    ShouldRetrieveGuestById() -> PASS

---
+++ Sheenam.Api.Test.Unit/Services/Foundations/Guests/GuestServiceTests.Logic.RetrieveById.cs
@@ -0,0 +1,44 @@
+﻿// ---------------------------------------------------
+// Copyright (c) Coalition of Good-hearted Engineers
+// Free to use to find comfort and pease
+// ---------------------------------------------------
+
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using Sheenam.Api.Models.Foundations.Guests;
+using Xunit;
+
+namespace Sheenam.Api.Test.Unit.Services.Foundations.Guests
+{
+    public partial class GuestServiceTests
+    {
+        [Fact]
+        public async Task ShouldRetrieveGuestById()
+        {
+            // given
+            Guid randomId = Guid.NewGuid();
+            Guest randomGuest = CreateRandomGuest();
+            Guest storageGuest = randomGuest;
+            Guest expectedGuest = storageGuest.DeepClone();
+
+            this.storageBrokerMock.Setup(broker => 
+                broker.SelectGuestByIdAsync(randomId))
+                .ReturnsAsync(storageGuest);
+            
+            // when
+            Guest actualGuest = 
+                await this.guestService.RetrieveGuestById(randomId);
+
+            // then 
+            actualGuest.Should().BeEquivalentTo(expectedGuest);
+
+            this.storageBrokerMock.Verify(broker => 
+                broker.SelectGuestByIdAsync(It.IsAny<Guid>()), 
+                Times.Once());
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
