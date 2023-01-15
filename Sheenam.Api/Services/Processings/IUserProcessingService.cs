7856710d98ee4489e9be450a90d010c5a62f279b
khatanboyevelbek <programmerelbek@gmail.com>
2023-01-15 18:34:19 +0500

    PROCESSINGS: CreateToken(User currentUser)

---
+++ Sheenam.Api/Services/Processings/IUserProcessingService.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------
+// Copyright (c) Coalition of Good-hearted Engineers
+// Free to use to find comfort and pease
+// ---------------------------------------------------
+
+using Sheenam.Api.Models.Foundations.Guests;
+using Host = Sheenam.Api.Models.Foundations.Hosts.Host;
+
+namespace Sheenam.Api.Services.Processings
+{
+    public interface IUserProcessingService
+    {
+        string CreateToken(Guest currentGuest);
+        string CreateToken(Host currentHost);
+    }
+}
---
+++ Sheenam.Api/Services/Processings/UserProcessingService.cs
@@ -0,0 +1,25 @@
+﻿// ---------------------------------------------------
+// Copyright (c) Coalition of Good-hearted Engineers
+// Free to use to find comfort and pease
+// ---------------------------------------------------
+
+using Sheenam.Api.Models.Foundations.Guests;
+using Sheenam.Api.Services.Foundations.Security;
+using Host = Sheenam.Api.Models.Foundations.Hosts.Host;
+
+namespace Sheenam.Api.Services.Processings
+{
+    public class UserProcessingService : IUserProcessingService
+    {
+        private readonly ISecurityService securityService;
+
+        public UserProcessingService(ISecurityService securityService) =>
+             this.securityService = securityService;
+
+        public string CreateToken(Guest currentGuest) => 
+            this.securityService.CreateToken(currentGuest);
+
+        public string CreateToken(Host currentHost) =>
+            this.securityService.CreateToken(currentHost);
+    }
+}
