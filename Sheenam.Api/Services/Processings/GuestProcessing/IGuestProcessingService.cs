95f73a2999f383d463f7230014cd35c82da8d87e
khatanboyevelbek <programmerelbek@gmail.com>
2023-01-21 13:47:03 +0500

    PROCESSINGS: Retrieve Guest By Cridentials

---
+++ Sheenam.Api/Services/Processings/GuestProcessing/IGuestProcessingService.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------
+// Copyright (c) Coalition of Good-hearted Engineers
+// Free to use to find comfort and pease
+// ---------------------------------------------------
+
+using Sheenam.Api.Models.Foundations.Guests;
+using Sheenam.Api.Models.Foundations.LoginModel;
+
+namespace Sheenam.Api.Services.Processings.GuestProcessing
+{
+    public interface IGuestProcessingService
+    {
+        Guest RetrieveGuestByCridentials(LoginModel loginModel);
+    }
+}
